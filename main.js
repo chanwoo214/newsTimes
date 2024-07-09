9c48f13935ecd67744c15e13b9ca62eb52e2547b
chanwoo214 <chanwooshim@gmail.com>
2024-07-10 01:48:03 +1200

    third commit change news

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const getLatestNews = async () => {
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
-    console.log("data", data.articles);
+    console.log("data", news);
 }
 getLatestNews();
 
