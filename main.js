cdabbbb8c9763dd5c9d0324573f6cd2fa49d3bfc
chanwoo214 <chanwooshim@gmail.com>
2024-07-10 16:28:02 +1200

    fourth commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const API_KEY = `80c8e3a6adbd41ba9dff4cc46dbc52c8`;
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`https://noona-times-v2.netlify.app/top-headlines`);
     console.log("URL", url);
     const response = await fetch(url);
     const data = await response.json();
